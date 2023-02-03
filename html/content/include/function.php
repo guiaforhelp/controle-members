412e305b7cdba76a96b98677774044619ed12d7f
guiaforhelp <contato@guiaforhelp.com.br>
2023-02-02 21:56:51 -0300

    modified:   html/content/header.php 	modified:   html/content/include/function.php 	modified:   html/content/include/objclass.php 	new file:   html/vendor/autoload.php 	new file:   html/vendor/composer/ClassLoader.php 	new file:   html/vendor/composer/LICENSE 	new file:   html/vendor/composer/autoload_classmap.php 	new file:   html/vendor/composer/autoload_namespaces.php 	new file:   html/vendor/composer/autoload_psr4.php 	new file:   html/vendor/composer/autoload_real.php 	new file:   html/vendor/composer/autoload_static.php

--- html/content/include/function.php
+++ html/content/include/function.php
@@ -9,3 +9,52 @@
 
 require 'objclass.php';
 require 'config.php';
+
+function selectMembers($date, $order){
+    
+    $selectM = new conectDB();
+    $selectM->selectDb('control_members', $date, $order);
+}
+
+function insertMembers(
+    $nome, 
+    $rg, 
+    $data_nascimento, 
+    $estado_civil, 
+    $naturalidade, 
+    $endereco, 
+    $n_endereco, 
+    $cep, 
+    $cidade, 
+    $data_batismo, 
+    $val_carteirinha, 
+    $situaco_membro, 
+    $cargo, 
+    $obs){
+    $insert = new conectDB();    
+    $insert->insertDb(
+        $nome,  
+        $rg, 
+        $data_nascimento, 
+        $estado_civil, 
+        $naturalidade, 
+        $endereco, 
+        $n_endereco, 
+        $cep, 
+        $cidade, 
+        $data_batismo, 
+        $val_carteirinha, 
+        $situaco_membro, 
+        $cargo, 
+        $obs);
+}
+
+function deleteMembers($date){
+    $delete = new conectDB();
+    $delete->deleteDb($date);
+}
+
+function updateMembers($id, $col, $date){
+    $update = new conectDB();
+    $update->updateDb($id, $col, $date);
+}
--- html/content/include/objclass.php
+++ html/content/include/objclass.php
@@ -20,13 +20,98 @@ class conectDB extends classMembres {
         .$this->getNameDb(), $this->getUserDb(), $this->getPassDb());
     }  
     
-    public function selectMembers(){        
-        $sql = $this->conect()->prepare("SELECT * FROM `control_members`");
+    public function selectDb($table, $date, $order){
+
+        $sql = $this->conect()->prepare("SELECT * FROM $table ORDER BY $date $order");
         $sql->execute();
-        $sql_member = $sql->fetchAll();
-        // print_r($sql_member);
+        $sql_member = $sql->fetchAll();      
+
         foreach($sql_member as $sql_members){
-            echo $sql_members['nome'].'<br>';
+            echo $sql_members[$date].'<br>';
         }
+    }
+
+    public function insertDb(
+        $member, 
+        $rg, 
+        $data_nascimento, 
+        $estado_civil, 
+        $naturalidade, 
+        $endereco, 
+        $n_endereco, 
+        $cep, 
+        $cidade, 
+        $data_batismo, 
+        $val_carteirinha, 
+        $situaco_membro, 
+        $cargo, 
+        $obs){
+        $insert = $this->conect()->prepare("INSERT INTO control_members (
+            nome, 
+            rg, 	
+            data_nascimento, 
+            estado_civil, 
+            naturalidade, 
+            endereco, 
+            n_endereco, 
+            cep, 
+            cidade, 
+            data_batismo, 
+            validade_carteirinha, 
+            situacao_membro, 
+            cargo, 
+            obs) VALUES(
+                :nome, 
+                :rg, 	
+                :data_nascimento, 
+                :estado_civil, 
+                :naturalidade, 
+                :endereco, 
+                :n_endereco, 
+                :cep, 
+                :cidade, 
+                :data_batismo, 
+                :validade_carteirinha, 
+                :situacao_membro, 
+                :cargo, 
+                :obs)");
+        
+        $query = "SELECT * FROM `control_members` WHERE `nome` LIKE '$member'";
+        $result = $this->conect()->query($query)->fetchAll();
+        
+        if(count($result) < 1){
+            $insert->execute(
+                array(
+                    ':nome' => $member,                    
+                    ':rg' => $rg, 	
+                    ':data_nascimento' => $data_nascimento, 
+                    ':estado_civil' => $estado_civil, 
+                    ':naturalidade' => $naturalidade, 
+                    ':endereco' => $endereco, 
+                    ':n_endereco' => $n_endereco, 
+                    ':cep' => $cep, 
+                    ':cidade' => $cidade, 
+                    ':data_batismo' => $data_batismo, 
+                    ':validade_carteirinha' => $val_carteirinha, 
+                    ':situacao_membro' => $situaco_membro, 
+                    ':cargo' => $cargo, 
+                    ':obs' => $obs
+                ));           
+        }else {
+            echo 'Usuario ja está cadastrado no sistema!';
+        }
+    }
+
+    public function deleteDb($date){
+        $delete = $this->conect()->prepare("DELETE FROM control_members WHERE id=:id");
+        $delete->execute(array(':id' => $date));
+    }
+
+    public function updateDb($id, $col, $date){
+        $update = $this->conect()->prepare("UPDATE `control_members` SET $col = :nome WHERE id=:id;");
+        $update->execute(array(
+            ':id' => $id,
+            ':nome' => $date
+        ));
     }
 }
